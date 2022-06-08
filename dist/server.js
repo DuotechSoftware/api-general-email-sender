9315a9f20e85afc7600876e57f98c7149d482e7b
Duosoft (Carlos Alpirez) <duotech.software@gmail.com>
2022-06-08 02:35:56 -0500

    Se agrego la config del .env

--- dist/server.js
+++ dist/server.js
@@ -1,4 +1,27 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -15,14 +38,16 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const body_parser_1 = __importDefault(require("body-parser"));
 const express_1 = __importDefault(require("express"));
 const mail_1 = __importDefault(require("@sendgrid/mail"));
+const dotenv = __importStar(require("dotenv"));
 const cors_1 = __importDefault(require("cors"));
 const tslog_1 = require("tslog"); // TODO: remove tslog dependency in production
+dotenv.config();
 const logger = new tslog_1.Logger();
 logger.info('Starting server');
 const app = (0, express_1.default)(); // create an instance of express
 app.use(body_parser_1.default.json()); // support json encoded bodies
 app.use((0, cors_1.default)());
-mail_1.default.setApiKey(process.env.SENDGRID_API_KEY || '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');
+mail_1.default.setApiKey(process.env.SENDGRID_API_KEY);
 logger.info('Starting server with api key: ' + process.env.SENDGRID_API_KEY);
 const port = process.env.PORT || 3000;
 // POST TO http://localhost:3000/send_email with body from example "request.json"
--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,18 @@
 import bodyParser from 'body-parser';
 import express from 'express';
 import client from '@sendgrid/mail';
+import * as dotenv from "dotenv";
 import cors from 'cors';
 import { Logger } from 'tslog'; // TODO: remove tslog dependency in production
 
+dotenv.config();
 const logger = new Logger();
 logger.info('Starting server');
 const app = express();  // create an instance of express
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(cors());
 
-client.setApiKey(process.env.SENDGRID_API_KEY || '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');
+client.setApiKey(process.env.SENDGRID_API_KEY);
 
 logger.info('Starting server with api key: ' + process.env.SENDGRID_API_KEY);
 
